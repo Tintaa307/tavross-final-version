ef1d10a6242ec19358851c2d011691801d830f21
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2023-06-28 14:03:11 -0300

    add page of token url

--- src/app/verify/[token]/page.tsx
+++ src/app/verify/[token]/page.tsx
@@ -2,9 +2,13 @@ import React from "react"
 
 const VerifyEmail = () => {
   return (
-    <div>
-      <h1>Validating...</h1>
-    </div>
+    <main className="w-full h-screen flex items-center justify-center">
+      <div className="w-[550px] h-[230px] bg-[#1f1f2e] rounded-lg">
+        <div>
+          <i className="ri-check-line" />
+        </div>
+      </div>
+    </main>
   )
 }
 
